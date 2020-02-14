2d3e18a49d7bf8ad8a938c8ac89d2b85a1c1dddc
xZeroW <vic_marts@hotmail.com>
2020-02-13 21:47:44 -0300

    first commit

---
+++ users/routes.config.js
@@ -0,0 +1,41 @@
+const UsersController = require('./controllers/users.controller');
+const PermissionMiddleware = require('../common/middlewares/auth.permission.middleware');
+const ValidationMiddleware = require('../common/middlewares/auth.validation.middleware');
+const config = require('../common/config/env.config');
+
+const ADMIN = config.permissionLevels.ADMIN;
+const PAID = config.permissionLevels.PAID_USER;
+const FREE = config.permissionLevels.NORMAL_USER;
+
+exports.routesConfig = function (app) {
+    // create user
+    app.post('/users', [
+        UsersController.insert
+    ]);
+    // list users
+    app.get('/users', [
+        ValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.minimumPermissionLevelRequired(ADMIN),
+        UsersController.list
+    ]);
+    // get user
+    app.get('/users/:userId', [
+        ValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.minimumPermissionLevelRequired(FREE),
+        PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
+        UsersController.getById
+    ]);
+    // update user
+    app.patch('/users/:userId', [
+        ValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.minimumPermissionLevelRequired(FREE),
+        PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
+        UsersController.patchById
+    ]);
+    // delete user
+    app.delete('/users/:userId', [
+        ValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.minimumPermissionLevelRequired(ADMIN),
+        UsersController.removeById
+    ]);
+};
