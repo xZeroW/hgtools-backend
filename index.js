1679e6bb96b7d1df9e671adfbf81ac423a16b57f
xZeroW <vic_marts@hotmail.com>
2020-02-29 19:52:28 -0300

    Update index.js
    oops

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
     res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-    if (req.method === 'OPTIONS' && req.ip === '::1') {
+    if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
     } else {
         return next();
